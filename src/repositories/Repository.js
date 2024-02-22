a42f909403ed9645854dff8e77088f119b48e792
louiscmm <louiscmm@gmail.com>
2024-02-22 01:00:23 +0000

    Add repository interface

---
+++ src/repositories/Repository.js
@@ -0,0 +1,14 @@
+//@ts-check
+/**
+ * Interface for classes that represent a repository.
+ *
+ * @interface Repository
+ */
+
+/**
+ * Add a new entity to the repository
+ *
+ * @function
+ * @name Repository#add
+ * @param {T} 
+ */
